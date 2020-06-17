51318704afb2e975a3c07ef3af09d81ed4ba56f5
weiyongyuan <weiyongyuan@fe-cred.com>
2020-06-17 17:42:20 +0800

    express 托管静态图片

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,11 @@ var router = express.Router();
 router.post('/login', IndexController.login);
 router.get('/user', IndexController.user);
 router.post("/adduser", IndexController.addUser);
-router.delete("/deleteuser", IndexController.deleteUser)
+router.delete("/deleteuser", IndexController.deleteUser);
+router.get("/jp", function (req, res) {
+    console.dir(req.originalUrl) // '/admin/new'
+    console.dir(req.baseUrl) // '/admin'
+    console.dir(req.protocol);
+    console.dir(req.xhr);
+})
 module.exports = router;
